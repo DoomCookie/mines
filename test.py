471a60ba8f97aefc89eb852a82a38d863da306e7
DoomCookie <xan232@gmail.com>
2020-12-29 12:34:12 +0300

    create field

---
+++ test.py
@@ -0,0 +1,23 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QGridLayout
+from PyQt5.QtWidgets import QSpacerItem, QVBoxLayout, QHBoxLayout, QLCDNumber
+from PyQt5.QtGui import QPainter, QColor, QBrush
+from PyQt5.QtCore import Qt, QPoint, QSize
+
+from PyQt5 import uic
+import sys
+
+class MyWidget(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('test.ui', self)
+        self.initUI()
+
+    def initUI(self):
+        print(self.btn.size())
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
